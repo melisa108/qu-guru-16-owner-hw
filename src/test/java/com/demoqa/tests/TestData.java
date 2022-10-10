d59624a9e0577fa42376d54ef392dac842804725
melisa108 <kagyulife@gmail.com>
2022-10-10 11:14:48 +0200

    added TestData

---
+++ src/test/java/com/demoqa/tests/TestData.java
@@ -0,0 +1,11 @@
+package com.demoqa.tests;
+
+public class TestData {
+    static String firstName = "Elon",
+            lastName = "Musk",
+            email = "elon@gmail.com",
+            phone = "1234567891",
+            day = "30",
+            month = "September",
+            year = "1965";
+}
