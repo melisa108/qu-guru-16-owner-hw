997a3c32630063d1f9e91e09daa22f8f2aff8590
melisa108 <kagyulife@gmail.com>
2023-01-05 17:19:19 +0100

    modified Testbase

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -37,7 +37,6 @@ public class TestBase {
         Configuration.browserSize = "1500x600";
         Configuration.holdBrowserOpen = false;
         Configuration.timeout = 10000;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
