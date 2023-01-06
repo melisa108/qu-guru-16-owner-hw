f0eebc024d501e7fa4c44bddaaa08be56a64e635
melisa108 <kagyulife@gmail.com>
2023-01-06 16:50:44 +0100

    added steps

--- src/test/java/com/demoqa/tests/RegistrationFormBestPracticiesTest.java
+++ src/test/java/com/demoqa/tests/RegistrationFormBestPracticiesTest.java
@@ -1,38 +1,56 @@
 package com.demoqa.tests;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.demoqa.pages.RegistrationFormPage;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
 
 public class RegistrationFormBestPracticiesTest extends TestBase {
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
 
     @Test
-    void fillFormTest() {
+    @DisplayName("Registration form with valid values")
+    public void fillFormTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
 
-        registrationFormPage.openPage()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setEmail(email)
-                .setGender(gender)
-                .setNumber(phone)
-                .setBirthDate(day, month, year)
-                .setSubject(subject)
-                .setHobbies(hobby)
-                .setPicture("img/" + picture)
-                .setCurrentAddress(currentAddress)
-                .setAddress(state, city)
-                .clickSubmit();
+        step("Открыть главную страницу", () -> {
 
-        registrationFormPage.checkResultsTableVisible()
-                .checkResult("Student Name", firstName + " " + lastName)
-                .checkResult("Student Email", email)
-                .checkResult("Gender", gender)
-                .checkResult("Mobile", phone)
-                .checkResult("Date of Birth", day + " " + month + "," + year)
-                .checkResult("Subjects", subject)
-                .checkResult("Hobbies", hobby)
-                .checkResult("Picture", picture)
-                .checkResult("Address", currentAddress)
-                .checkResult("State and City", state + " " + city);
+            registrationFormPage.openPage();
+        });
+
+        step("Заполнить все поля валидными данными", () -> {
+            registrationFormPage.setFirstName(firstName)
+                    .setLastName(lastName)
+                    .setEmail(email)
+                    .setGender(gender)
+                    .setNumber(phone)
+                    .setBirthDate(day, month, year)
+                    .setSubject(subject)
+                    .setHobbies(hobby)
+                    .setPicture("img/" + picture)
+                    .setCurrentAddress(currentAddress)
+                    .setAddress(state, city);
+        });
+
+        step("Нажать кнопку Submit", () -> {
+            registrationFormPage.clickSubmit();
+        });
+
+        step("Проверить поля в открывшийся форме", () -> {
+            registrationFormPage.checkResultsTableVisible()
+                    .checkResult("Student Name", firstName + " " + lastName)
+                    .checkResult("Student Email", email)
+                    .checkResult("Gender", gender)
+                    .checkResult("Mobile", phone)
+                    .checkResult("Date of Birth", day + " " + month + "," + year)
+                    .checkResult("Subjects", subject)
+                    .checkResult("Hobbies", hobby)
+                    .checkResult("Picture", picture)
+                    .checkResult("Address", currentAddress)
+                    .checkResult("State and City", state + " " + city);
+        });
     }
 }
